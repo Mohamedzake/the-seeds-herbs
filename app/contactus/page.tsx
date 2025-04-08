0bfc20d0a51d56fdcd18ca93efa9ddb96bb3fc2f
Mohamedzake <zakem7671@gmail.com>
2025-04-08 18:26:53 +0200

    j

--- app/contactus/page.tsx
+++ app/contactus/page.tsx
@@ -5,17 +5,15 @@ export default function Page() {
   return (
     <section>
       <div className="bg-home_bg bg-cover bg-no-repeat bg-center w-full aspect-square h-[40vh] md:h-[60vh] relative">
-        <div className="flex flex-col justify-center items-start h-full text-left">
-          <div className="text-white flex flex-col items-start gap-y-4 px-4 pr-16 md:px-6 md:pr-24 lg:px-8 lg:pr-32 xl:px-10 xl:pr-40 w-full max-w-[1200px] mx-auto animate-fadeIn3D">
-            <h1 className="text-left text-lg sm:text-xl md:text-2xl lg:text-3xl font-semibold leading-snug">
+        <div className="flex flex-col justify-center items-center h-full text-center">
+          <div className="text-white flex flex-col items-center gap-y-4 px-4 w-full max-w-[1200px] mx-auto animate-fadeIn3D">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-snug">
               Contact Us
             </h1>
-            <h2 className="text-left text-xs sm:text-sm md:text-base lg:text-lg max-w-xl">
-              Giza has been a global market leader for more than 90 years
-            </h2>
           </div>
         </div>
-      </div>{" "}
+      </div>
+
       <ContactForm />
     </section>
   );
--- components/WhatsApp.tsx
+++ components/WhatsApp.tsx
@@ -5,7 +5,7 @@ import { IoLogoWhatsapp } from "react-icons/io5";
 const WhatsApp = () => {
   return (
     <Link
-      href="https://wa.me/966920031014"
+      href="https://wa.me/201026450957"
       className="fixed bottom-4 right-4 text-4xl duration-75 text-white bg-primary-green2 z-50 cursor-pointer py-3 px-3 rounded-full"
     >
       <IoLogoWhatsapp />
